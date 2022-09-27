087c064569f912a81b218c6e46a13fdd24535452
Sergio Arias <sarias@unal.edu.co>
2022-09-27 09:41:26 -0500

    intento 2

---
+++ src/main/java/gestion/zooAnimales/Anfibio.java
@@ -0,0 +1,99 @@
+package zooAnimales;
+
+import gestion.Zona;
+
+import java.util.ArrayList;
+
+public class Anfibio extends Animal {
+
+    private static ArrayList<Anfibio> listado = new ArrayList<>();
+    public static int ranas;
+    public static int salamandras;
+    private String colorPiel;
+    private boolean venenoso;
+
+    public Anfibio(String nombre, int edad, String habitat, String genero, Zona zona, String colorPiel, Boolean venenoso) {
+        super(nombre, edad, habitat, genero, zona);
+        this.colorPiel = colorPiel;
+        this.venenoso = venenoso;
+    }
+
+    public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, Boolean venenoso) {
+        super(nombre, edad, habitat, genero);
+        this.colorPiel = colorPiel;
+        this.venenoso = venenoso;
+        Anfibio.listado.add(this);
+    }
+
+    public Anfibio(){
+        Anfibio.listado.add(this);
+    }
+
+    public static int cantidadAnfibios(){
+        if (Anfibio.listado != null){
+            return Anfibio.listado.size();
+        }
+        else{
+            return 0;
+        }
+    }
+
+    @Override
+    public String movimiento() {
+        return "saltar";
+    }
+
+    public static Anfibio crearRana(String nombre, int edad, String genero){
+        Anfibio rana = new Anfibio(nombre, edad, "selva", genero, "rojo", true);
+        Anfibio.listado.add(rana);
+        Anfibio.ranas++;
+        return rana;
+    }
+
+    public static Anfibio crearSalamandra(String nombre, int edad, String genero){
+        Anfibio salamandra = new Anfibio(nombre, edad, "selva", genero, "negro y amarillo", false);
+        Anfibio.listado.add(salamandra);
+        Anfibio.salamandras++;
+        return salamandra;
+    }
+
+    public static ArrayList<Anfibio> getListado() {
+        return listado;
+    }
+
+    public static void setListado(ArrayList<Anfibio> listado) {
+        Anfibio.listado = listado;
+    }
+
+    public static int getRanas() {
+        return ranas;
+    }
+
+    public static void setRanas(int ranas) {
+        Anfibio.ranas = ranas;
+    }
+
+    public static int getSalamandras() {
+        return salamandras;
+    }
+
+    public static void setSalamandras(int salamandras) {
+        Anfibio.salamandras = salamandras;
+    }
+
+    public String getColorPiel() {
+        return colorPiel;
+    }
+
+    public void setColorPiel(String colorPiel) {
+        this.colorPiel = colorPiel;
+    }
+
+    public boolean isVenenoso() {
+        return venenoso;
+    }
+
+    public void setVenenoso(boolean venenoso) {
+        this.venenoso = venenoso;
+    }
+}
---
+++ src/main/java/gestion/zooAnimales/Reptil.java
@@ -0,0 +1,99 @@
+package zooAnimales;
+
+import gestion.Zona;
+
+import java.util.ArrayList;
+
+public class Reptil extends Animal {
+
+    private static ArrayList<Reptil> listado = new ArrayList<>();
+    public static int iguanas;
+    public static int serpientes;
+    private String colorEscamas;
+    private int largoCola;
+
+    public Reptil(String nombre, int edad, String habitat, String genero, Zona zona, String colorEscamas, int largoCola) {
+        super(nombre, edad, habitat, genero, zona);
+        this.colorEscamas = colorEscamas;
+        this.largoCola = largoCola;
+    }
+
+    public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
+        super(nombre, edad, habitat, genero);
+        this.colorEscamas = colorEscamas;
+        this.largoCola = largoCola;
+        Reptil.listado.add(this);
+    }
+
+    public Reptil(){
+        Reptil.listado.add(this);
+    };
+
+    public static int cantidadReptiles(){
+        if (Reptil.listado != null){
+            return Reptil.listado.size();
+        }
+        else{
+            return 0;
+        }
+    }
+
+    @Override
+    public String movimiento() {
+        return "reptar";
+    }
+
+    public static Reptil crearIguana(String nombre, int edad, String genero){
+        Reptil iguana = new Reptil(nombre, edad, "humedal", genero, "verde", 3);
+        Reptil.listado.add(iguana);
+        Reptil.iguanas++;
+        return iguana;
+    }
+
+    public static Reptil crearSerpiente(String nombre, int edad, String genero){
+        Reptil serpiente = new Reptil(nombre, edad, "jungla", genero, "blanco", 1);
+        Reptil.listado.add(serpiente);
+        Reptil.serpientes++;
+        return serpiente;
+    }
+
+    public static ArrayList<Reptil> getListado() {
+        return listado;
+    }
+
+    public static void setListado(ArrayList<Reptil> listado) {
+        Reptil.listado = listado;
+    }
+
+    public static int getIguanas() {
+        return iguanas;
+    }
+
+    public static void setIguanas(int iguanas) {
+        Reptil.iguanas = iguanas;
+    }
+
+    public static int getSerpientes() {
+        return serpientes;
+    }
+
+    public static void setSerpientes(int serpientes) {
+        Reptil.serpientes = serpientes;
+    }
+
+    public String getColorEscamas() {
+        return colorEscamas;
+    }
+
+    public void setColorEscamas(String colorEscamas) {
+        this.colorEscamas = colorEscamas;
+    }
+
+    public int getLargoCola() {
+        return largoCola;
+    }
+
+    public void setLargoCola(int largoCola) {
+        this.largoCola = largoCola;
+    }
+}
