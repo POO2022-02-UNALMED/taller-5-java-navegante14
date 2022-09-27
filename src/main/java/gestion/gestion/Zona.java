e1837d4497407e9820418bae4de3702a734a4b57
Sergio Arias <sarias@unal.edu.co>
2022-09-27 09:47:03 -0500

    intento 3

--- src/main/java/gestion/gestion/Zona.java
+++ src/main/java/gestion/gestion/Zona.java
@@ -1,49 +1,57 @@
 package gestion;
-import zooAnimales.Animal; 
+
+import zooAnimales.Animal;
+
 import java.util.ArrayList;
 
 public class Zona {
-	private String nombre;
-	private Zoologico zoo;
-	private ArrayList<Animal> animales= new ArrayList<Animal>();
-	
-	
-	public Zona() {
-		
-	}
-	public Zona(String nombre, Zoologico zoo) {
-		this.nombre=nombre;
-		this.zoo=zoo;
-	}
-	public Zona(String nombre, Zoologico zoo, ArrayList<Animal> animales) {
-		this.nombre=nombre;
-		this.zoo=zoo;
-		this.animales=animales;
-	}
-	
-	public void agregarAnimales(Animal animales) {
-		this.animales.add(animales);
-	}
-	public int cantidadAnimales() {
-		return this.animales.size();
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public Zoologico getZoo() {
-		return zoo;
-	}
-	public void setZoo(Zoologico zoo) {
-		this.zoo = zoo;
-	}
-	public ArrayList<Animal> getAnimales() {
-		return animales;
-	}
-	public void setAnimales(ArrayList<Animal> animales) {
-		this.animales = animales;
-	}
+    private String nombre;
+    private Zoologico zoo;
+    private ArrayList<Animal> animales = new ArrayList<>();
+
+    public Zona() {}
+
+    public Zona(String nombre, Zoologico zoo, ArrayList<Animal> animales) {
+        this.nombre = nombre;
+        this.zoo = zoo;
+        this.animales = animales;
+    }
+
+    public Zona(String nombre, Zoologico zoo) {
+        this.nombre = nombre;
+        this.zoo = zoo;
+    }
+
+
+    public void agregarAnimales(Animal animal){
+        this.animales.add(animal);
+    }
+
+    public int cantidadAnimales(){
+        return this.animales.size();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Zoologico getZoo() {
+        return this.zoo;
+    }
+
+    public void setZoo(Zoologico zoo) {
+        this.zoo = zoo;
+    }
+
+    public ArrayList<Animal> getAnimales() {
+        return animales;
+    }
+
+    public void setAnimales(ArrayList<Animal> animales) {
+        this.animales = animales;
+    }
 }
--- src/main/java/gestion/gestion/Zoologico.java
+++ src/main/java/gestion/gestion/Zoologico.java
@@ -1,50 +1,59 @@
 package gestion;
-import java.util.*;
+
+import java.util.ArrayList;
 
 public class Zoologico {
-	private String nombre;
-	private String ubicacion;
-	private ArrayList<Zona> zonas= new ArrayList<Zona>();
-	
-	public Zoologico() {
-		
-	}
-	public Zoologico(String nombre, String ubicacion) {
-		this.nombre=nombre;
-		this.ubicacion=ubicacion;
-	}
-	public Zoologico(String nombre, String ubicacion, ArrayList<Zona> zonas) {
-		this.nombre=nombre;
-		this.ubicacion=ubicacion;
-		this.zonas=zonas;
-	}
-	public void agregarZonas(Zona zonas) {
-		this.zonas.add(zonas);
-	}
-	public int cantidadTotalAnimales() {
-		int total=0;
-		for (Zona i: this.zonas) {
-			total+=i.cantidadAnimales();
-		}
-		return total;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getUbicacion() {
-		return ubicacion;
-	}
-	public void setUbicacion(String ubicacion) {
-		this.ubicacion = ubicacion;
-	}
-	public ArrayList<Zona> getZonas() {
-		return zonas;
-	}
-	public void setZonas(ArrayList<Zona> zonas) {
-		this.zonas = zonas;
-	}
-	
+     private String nombre;
+     private String ubicacion;
+     private ArrayList<Zona> zona = new ArrayList<>();
+
+     public Zoologico(){}
+
+     public Zoologico(String nombre, String ubicacion, ArrayList<Zona> zona) {
+          this.nombre = nombre;
+          this.ubicacion = ubicacion;
+          this.zona = zona;
+     }
+
+     public Zoologico(String nombre, String ubicacion) {
+          this.nombre = nombre;
+          this.ubicacion = ubicacion;
+     }
+
+     public void agregarZonas(Zona zona){
+          this.zona.add(zona);
+     }
+
+     public int cantidadTotalAnimales(){
+
+          int cant = 0;
+          for (Zona zona: this.zona) {
+               cant += zona.cantidadAnimales();
+          }
+          return cant;
+     }
+
+     public String getNombre() {
+          return nombre;
+     }
+
+     public void setNombre(String nombre) {
+          this.nombre = nombre;
+     }
+
+     public String getUbicacion() {
+          return ubicacion;
+     }
+
+     public void setUbicacion(String ubicacion) {
+          this.ubicacion = ubicacion;
+     }
+
+     public ArrayList<Zona> getZona() {
+          return zona;
+     }
+
+     public void setZona(ArrayList<Zona> zona) {
+          this.zona = zona;
+     }
 }
